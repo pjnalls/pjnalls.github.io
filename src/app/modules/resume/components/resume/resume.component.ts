a859af6ed0e50227d3b1144ff777ee2eb6a17418
Preston <preston.james.nalls@gmail.com>
2019-01-31 06:25:27 -0600

    Added copy of BioComponent as bolierplate code for new ResumeComponent.

---
+++ src/app/modules/resume/components/resume/resume.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'resume-page',
+    templateUrl: './resume.component.html',
+    styleUrls: ['./resume.component.scss']
+})
+
+export class ResumeComponent {}
---
+++ src/app/modules/resume/resume-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ResumeComponent } from './components/resume/resume.component';
+
+const appRoutes: Routes = [
+    { path: '', component: ResumeComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+
+export class ResumeRoutingModule {}
