cc94ef7c96a03d00b32ef075c39ae3ece490de3a
Preston <preston.james.nalls@gmail.com>
2019-02-16 08:13:44 -0600

    Remove duplicate TypeScript code copied from projects.component.ts

--- src/app/modules/contact/components/contact/contact.component.ts
+++ src/app/modules/contact/components/contact/contact.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild, Renderer2, OnInit, Inject, ElementRef } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'contact-page',
@@ -7,33 +6,4 @@ import { DOCUMENT } from '@angular/common';
     styleUrls: ['./contact.component.scss']
 })
 
-export class ContactComponent implements OnInit{
-    anchorDate: Date = new Date('December 25, 2018');
-    today: Date = new Date();
-    diff: number = Math.abs(this.today.getTime() - this.anchorDate.getTime());
-    dayDiff: number = Math.ceil(this.diff/(1000 * 60 * 60 * 24));
-
-    constructor(private _renderer2: Renderer2, @Inject(DOCUMENT) private _document: any) {}
-    @ViewChild('calendar') calendar: ElementRef
-
-    public ngOnInit() {
-        
-        let s = this._renderer2.createElement('script');
-
-        s.type = 'text/javascript';
-        s.src = 'src/vendor/IonicaBizau/github-calendar/dist/github-calendar.min.js';
-        this._renderer2.appendChild(this.calendar.nativeElement, s);
-
-        s = this._renderer2.createElement('link');
-
-        s.rel = 'stylesheet';
-        s.href='src/vendor/IonicaBizau/github-calendar/dist/github-calendar.css';
-        this._renderer2.appendChild(this.calendar.nativeElement, s);
-        
-        s = this._renderer2.createElement('link');
-
-        s.rel = 'stylesheet';
-        s.href='src/vendor/IonicaBizau/github-calendar/dist/github-calendar-responsive.css';
-        this._renderer2.appendChild(this.calendar.nativeElement, s);
-    }
-}
+export class ContactComponent {}
