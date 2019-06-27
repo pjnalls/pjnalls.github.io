af7d7e333a63cabefa13898d1edb9d2166a33ec4
Preston <preston.james.nalls@gmail.com>
2019-06-27 05:25:28 -0500

    Add language service as a provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './shared/components/home/home.component';
+import { LanguageService } from './language.service';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
@@ -17,7 +18,7 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
     providers: [{
         provide: LocationStrategy,
         useClass: HashLocationStrategy
-    }],
+    }, LanguageService],
     bootstrap: [
         AppComponent
     ]
