5a112e8b00fd3c622ed6d51b0b0abc8233737440
Preston <preston.james.nalls@gmail.com>
2019-06-27 05:24:51 -0500

    Add language service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { routerTransition } from './animations';
+import { LanguageService } from './language.service';
 
 @Component({
     selector: 'app-root',
+    providers: [LanguageService],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     animations: [
@@ -12,15 +14,39 @@ import { routerTransition } from './animations';
 
 export class AppComponent {
     name: string = 'Preston Nalls';
-    lang: string = 'en';
+
+    
+    constructor(private languageService: LanguageService) {}
+    language: string = 'en';
+    
+    ngOnInit(): void {
+        this.languageService.languageSetting$
+            .subscribe(
+                language => {
+                    this.languageService.setLanguage(language);
+                }
+            );
+    }
 
     // Change language settings to Japanese
     changeLanguageToJp() {
-        this.lang = 'jp'
+        this.language = 'jp';
+        this.languageService.languageSetting$
+            .subscribe(
+                language => {
+                    this.languageService.setLanguage(language);
+                }
+            );
     }
 
     // Change language settings to English
     changeLanguageToEn() {
-        this.lang = 'en'
+        this.language = 'en'
+        this.languageService.languageSetting$
+            .subscribe(
+                language => {
+                    this.languageService.setLanguage(language);
+                }
+            );
     }
 }
