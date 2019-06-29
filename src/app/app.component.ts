15e37d51ae89c23404efaef6bd73efe8d0eda57b
Preston <preston.james.nalls@gmail.com>
2019-06-29 06:40:59 -0500

    Use 'DoCheck' hook to keep updating language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck} from '@angular/core';
 import { routerTransition } from './animations';
 import { LanguageService } from './language.service';
 
@@ -12,24 +12,29 @@ import { LanguageService } from './language.service';
     ]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
     name: string = 'Preston Nalls';
     language: string = 'en';
     
-    constructor(private languageService: LanguageService) {
-        this.languageService.setLanguage(this.language)
+    constructor(private languageService: LanguageService) {}
+
+    ngOnInit() {
+        this.language = 'en';
     }
-    
 
     // Change language settings to Japanese
     changeLanguageToJp() {
         this.language = 'jp';
-        this.languageService.setLanguage(this.language)
+        this.name = 'プレストン・ノールズ';
     }
 
     // Change language settings to English
     changeLanguageToEn() {
         this.language = 'en';
+        this.name = 'Preston Nalls';
+    }
+
+    ngDoCheck() {
         this.languageService.setLanguage(this.language);
     }
 }
