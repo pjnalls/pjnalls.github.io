b5c8a1ea3420a8f3a787ad9889b5aaa974a3ce94
pjnalls <42269808+pjnalls@users.noreply.github.com>
2022-09-17 12:24:49 -0500

    Disable link to Digital Rain demo.

--- src/components/portfolio/Projects.tsx
+++ src/components/portfolio/Projects.tsx
@@ -29,7 +29,7 @@ export default function  Projects() {
         <ProjectItem
             title="pjnalls.com"
             description="My portfolio and blog website made seemlessly thanks to Astro with React integrations."
-            madeWith="Astro , React, Next.js, TypeScript, Tailwind CSS, and ❤️."
+            madeWith="Astro, React, Next.js, TypeScript, Tailwind CSS, and ❤️."
             backgroundImg={pDot}
             enableScreenshot={false}
             enableCode={false}
@@ -106,9 +106,9 @@ export default function  Projects() {
             backgroundImg={digitalRain}
             enableScreenshot={false}
             enableCode={true}
-            enableDemo={true}
-            demoLabel="View Project"
-            demoUrl="/digital-rain/index.html"
+            enableDemo={false}
+            demoLabel=""
+            demoUrl=""
             githubUrl="https://github.com/pjnalls/DigitalRain"
           />
           <ProjectItem
