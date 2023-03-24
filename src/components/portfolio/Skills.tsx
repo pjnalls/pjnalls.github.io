fb507bbc7e9187d432758008638d153fb93d4c8e
pjnalls <42269808+pjnalls@users.noreply.github.com>
2023-03-24 12:32:19 -0500

    Add ReactiveX icon for RxJS skill.

--- src/components/portfolio/Skills.tsx
+++ src/components/portfolio/Skills.tsx
@@ -30,6 +30,7 @@ import {
   SiNx,
   SiPandas,
   SiPython,
+  SiReactivex,
   SiRedux,
   SiSwagger,
   SiTailwindcss,
@@ -65,6 +66,25 @@ export default function Skills() {
                 </div>
                 <div className="bg-[#1f2937] w-full h-[0.6rem] ml-3">
                   <div className="bg-[#9d8c66] w-[96%] h-[0.6rem]"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow dark:shadow-slate-800 dark:shadow-md rounded-xl hover:scale-110 cursor-pointer ease transition-all ease-in duration-200">
+            <div className="grid grid-cols-1 gap-4 justify-between items-center">
+              <div className="m-auto inline-flex justify-around items-center w-full">
+                <SiReactivex
+                  size="48px"
+                  className="mr-6 text-[#910038] dark:text-[#e10988] dark:bg-transparent rounded-md"
+                />
+                <h3>RxJS</h3>
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="text-slate-600 dark:text-slate-300 text-lg font-semibold">
+                  93%{" "}
+                </div>
+                <div className="bg-[#1f2937] w-full h-[0.6rem] ml-3">
+                  <div className="bg-[#9d8c66] w-[93%] h-[0.6rem]"></div>
                 </div>
               </div>
             </div>
