308ae83a0d45b311b150ef453fa89313c1ef4a4f
pjnalls <42269808+pjnalls@users.noreply.github.com>
2023-07-13 17:55:27 -0500

    Complete `Main` and `Footer` localization.

--- public/localize.js
+++ public/localize.js
@@ -7,9 +7,9 @@
     );
     for (let i = 0; i < intlTags?.length; i++) {
       intlTags.item(i).textContent =
-        localizationDB[intlTags.item(i).getAttribute("id")].get(
+        localizationDB[intlTags.item(i).getAttribute("id")]?.get(
           localStorage.getItem("locale")
-        ) ?? localizationDB[intlTags.item(i).getAttribute("id")].get("en-US");
+        ) ?? localizationDB[intlTags.item(i).getAttribute("id")]?.get("en-US");
     }
   };
 
@@ -17,6 +17,63 @@
    * LOCALIZATION DATABASE
    */
   var localizationDB = {
+    /**
+     * NAVIGATION BAR
+     */
+    // about: new Map([
+    //   ["en-US", "About"],
+    //   ["ja", "自己紹介"],
+    //   ["zh-TW", "自我介紹"],
+    //   ["zh-CN", "自我介绍"],
+    //   ["es-ES", "Introducción"],
+    //   ["ko", "자기소개"],
+    //   ["hi", "आत्म परिचय"],
+    // ]),
+    // skills: new Map([
+    //   ["en-US", "Skills"],
+    //   ["ja", "スキル"],
+    //   ["zh-TW", "技能"],
+    //   ["zh-CN", "技能"],
+    //   ["es-ES", "Técnica"],
+    //   ["ko", "기술"],
+    //   ["hi", "कौशल"],
+    // ]),
+    // works: new Map([
+    //   ["en-US", "Works"],
+    //   ["ja", "作品"],
+    //   ["zh-TW", "作品"],
+    //   ["zh-CN", "作品"],
+    //   ["es-ES", "Obras"],
+    //   ["ko", "작품"],
+    //   ["hi", "काम"],
+    // ]),
+    // blog: new Map([
+    //   ["en-US", "Blog"],
+    //   ["ja", "ブログ"],
+    //   ["zh-TW", "博客"],
+    //   ["zh-CN", "博客"],
+    //   ["es-ES", "Blog"],
+    //   ["ko", "블로그"],
+    //   ["hi", "ब्लॉग"],
+    // ]),
+    // resume: new Map([
+    //   ["en-US", "Resume"],
+    //   ["ja", "レジュメ"],
+    //   ["zh-TW", "簡歷"],
+    //   ["zh-CN", "简历"],
+    //   ["es-ES", "CV (EN)"],
+    //   ["ko", "이력서"],
+    //   ["hi", "सीवी (अंग्रेजी)"],
+    // ]),
+    // cv: new Map([
+    //   ["en-US", "CV (JA)"],
+    //   ["ja", "履歴書"],
+    //   ["zh-TW", "自我介紹"],
+    //   ["zh-CN", "自我介绍"],
+    //   ["es-ES", "CV (JA)"],
+    //   ["ko", "자기소개"],
+    //   ["hi", "सीवी (जापानी)"],
+    // ]),
     /**
      * "Hi 👋, I'm Preston."
      */
@@ -27,7 +84,7 @@
       ["zh-CN", "大家好 👋、 我叫"],
       ["es-ES", "Hola 👋, soy"],
       ["ko", "안녕하세요👋, 저는 "],
-      ["hi", "नमस्ते 👋, मेरा नाम "],
+      ["hi", "नमस्ते 🙏, मेरा नाम "],
     ]),
     preston: new Map([
       ["en-US", "Preston"],
@@ -44,8 +101,8 @@
       ["zh-TW", "。"],
       ["zh-CN", "。"],
       ["es-ES", "."],
-      ["ko", "이에요"],
-      ["hi", "है"],
+      ["ko", " 이에요"],
+      ["hi", " है"],
     ]),
     /**
      * "Polyglot Frontend Engineer"
@@ -68,7 +125,7 @@
         the STEAM (Scientific, Technologic, Engineering, 
         Artistic, and Mathematic) disciplines to cultivate 
         better analytical and creative abilities for both 
-        professional and personal projects."
+        professional and personal works."
      */
     summary1: new Map([
       [
@@ -77,21 +134,21 @@
       ],
       [
         "ja",
-        "僕の焦点は、美的で機能的なデザインと、効率的でエレガントなフロントエンド・コードを組み合わせることです。",
+        "目標は、美的で機能的なデザインと、効率的でエレガントなフロントエンド・コードを組み合わせることです。",
       ],
-      ["zh-TW", `我的工作重點是用戶體驗工程的前端設計和開發。`],
-      ["zh-CN", `我的工作重点是用户体验工程的前端设计和开发。`],
+      ["zh-TW", `我的目標是用戶體驗工程的前端設計和開發。`],
+      ["zh-CN", `我的目标是用户体验工程的前端设计和开发。`],
       [
         "es-ES",
         "Mi objetivo es combinar un diseño estético y funcional con un código frontend eficiente y elegante.",
       ],
-      // ["ko", "다국어 프론트엔드 엔지니어"],
-      // ["hi", "पॉलीग्लॉट फ्रंटएंड इंजीनियर"],
+      ["ko", "목표는 효율적이고 우아한 프론트엔드 디자인과 코드입니다."],
+      ["hi", "मेरा लक्ष्य सर्वोत्तम फ्रंटएंड कोड और डिज़ाइन है"],
     ]),
     summary2: new Map([
       [
         "en-US",
-        "I also focus on continually learning in the STEAM (Scientific, Technologic, Engineering, Artistic, and Mathematic) disciplines to cultivate better analytical and creative abilities for both professional and personal projects.",
+        "I also focus on continually learning in the STEAM (Scientific, Technologic, Engineering, Artistic, and Mathematic) disciplines to cultivate better analytical and creative abilities for both professional and personal works.",
       ],
       [
         "ja",
@@ -109,8 +166,14 @@
         "es-ES",
         "También me centro en aprender continuamente en las disciplinas STEAM (Scientific, Technologic, Engineering, Artistic, and Mathematic) para cultivar mejores habilidades analíticas y creativas tanto para proyectos profesionales como personales.",
       ],
-      // ["ko", "다국어 프론트엔드 엔지니어"],
-      // ["hi", "पॉलीग्लॉट फ्रंटएंड इंजीनियर"],
+      [
+        "ko",
+        "또한 STEAM(과학, 기술, 공학, 예술, 수학) 분야의 지속적인 학습에 중점을 두어 업무와 개인 프로젝트에서 더 나은 분석력과 창의력을 키울 수 있다.",
+      ],
+      [
+        "hi",
+        "मैं STEAM (विज्ञान, प्रौद्योगिकी, इंजीनियरिंग, कला, गणित) सीख रहा हूँ",
+      ],
     ]),
     madeWith: new Map([
       ["en-US", "Made with"],
