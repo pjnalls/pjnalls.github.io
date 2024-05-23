717e2e094a098b16ce8ecee9d877f6f07827d20e
pjnalls <42269808+pjnalls@users.noreply.github.com>
2024-05-23 13:53:06 -0500

    🆙 content(about): update text on About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,11 +12,7 @@ function About() {
       exit={{ marginTop: 120, opacity: 0, scale: 0.96 }}
       transition={{ duration: 0.5, ease: 'easeOut' }}
     >
-      <Container
-        mx={0}
-        px={24}
-        size={'xs'}
-      >
+      <Container mx={0} px={24} size={'xs'}>
         <Title fw={500} order={2} ta={'left'} my={'lg'}>
           🧑‍💻 About me
         </Title>
@@ -27,17 +23,21 @@ function About() {
           <br />
           <br />
           👨‍💻 I create, enhance, and maintain React, Angular, .NET, and React
-          Native web and mobile apps and services where I work and do extended
-          amounts of yoga in my spare time.
+          Native web and mobile apps and services where I work, and I like
+          listening to a lot Square Enix music and reading the lastest tech news
+          in my free time.
           <br />
-          I primarily write code in C# and TypeScript, currently learning
-          Python, and can read in multiple natural languages.
+          I primarily write code in C# and TypeScript and can read in multiple
+          natural languages.
           <br />
           <br />
-          🧬 Hopefully through my work, I can inspire others to make their work
-          "human-centered"—as human-computer interaction (HCI) becomes of
-          paramount importance with advancements in technologies such as artificial general
-          intelligence (AGI) and metaverse-user experiences.
+          🦄 I consider myself to be a bit of a unicorn in the sense that I'm
+          currently a fullstack web and mobile developer where I work yet I have
+          a background in art and design: I grew up with a passion in drawing,
+          anime, and games. I also graduated from a university with a Bachelor
+          of Science degree with a concentration in 2D and 3D animation and,
+          later on, with an additional Bachelor of Science degree in computer
+          science.
           <br />
           <br />
           💼 Please feel free to look around while you're here.
