80ae73e764df6868fba817fc202e3a69ca6df485
pjnalls <42269808+pjnalls@users.noreply.github.com>
2024-10-14 16:25:21 -0500

    🆕 content(skills): add new certification ✨

--- src/shared/fixtures/Cert.fixtures.ts
+++ src/shared/fixtures/Cert.fixtures.ts
@@ -1,52 +1,59 @@
 import { CertProps } from '@/src/shared/types';
 
 export const certs: CertProps[] = [
-	{
-		alt: 'Completed frontend framework certification',
-		name: 'Certified React Developer',
-		description: 'Verify completion at',
-		issuer: 'W3Schools',
-		imgUrl: '/assets/certifications/react-cert.avif',
-		certUrl: 'https://verify.w3schools.com/1OE1S9OIYK',
-	},
-	{
-		alt: 'Completed statically-typed scripting language certification',
-		name: 'Certified TypeScript Developer',
-		description: 'Verify completion at',
-		issuer: 'W3Schools',
-		imgUrl: '/assets/certifications/typescript-cert.avif',
-		certUrl: 'https://verify.w3schools.com/1OE3O8LR7B',
-	},
-	{
-		alt: 'Completed stylesheet framework certification',
-		name: 'Sass Course Certificate of Completion',
-		description: 'Verify completion at',
-		issuer: 'Codecademy',
-		imgUrl: '/assets/certifications/sass-cert.avif',
-		certUrl:
-			'https://www.codecademy.com/profiles/pjnalls/certificates/eb1ffda40f347629dcef6de33d3f9741',
-	},
-	{
-		alt: 'Completed scripting language certification',
-		name: 'Certified JavaScript Developer',
-		description: 'Verify completion at',
-		issuer: 'W3Schools',
-		imgUrl: '/assets/certifications/javascript-cert.avif',
-		certUrl: 'https://verify.w3schools.com/1ODZHSV1OV',
-	},
-	{
-		alt: 'Completed Japanese certification',
-		name: 'N2 Certificate Japanese-Language Proficiency',
-		description: 'Verify completion with ID "N2A156406A"',
-		issuer: 'The Japan Foundation / Japan Educational Exchanges and Services',
-		imgUrl: '/assets/certifications/japanese-cert.avif',
-	},
-	{
-		alt: 'Completed Chinese certification',
-		name: 'Certificate of Achievement for Chinese',
-		description:
-			'for successfully completing 40 hours of Chinese lessons with certified Preply tutors.',
-		issuer: 'Preply',
-		imgUrl: '/assets/certifications/chinese-cert.avif',
-	},
+  {
+    alt: 'Completed frontend framework certification',
+    name: 'Certified React Developer',
+    description: 'Verify completion at',
+    issuer: 'W3Schools',
+    imgUrl: '/assets/certifications/react-cert.avif',
+    certUrl: 'https://verify.w3schools.com/1OE1S9OIYK',
+  },
+  {
+    alt: 'Completed statically-typed scripting language certification',
+    name: 'Certified TypeScript Developer',
+    description: 'Verify completion at',
+    issuer: 'W3Schools',
+    imgUrl: '/assets/certifications/typescript-cert.avif',
+    certUrl: 'https://verify.w3schools.com/1OE3O8LR7B',
+  },
+  {
+    alt: 'Completed stylesheet framework certification',
+    name: 'Sass Course Certificate of Completion',
+    description: 'Verify completion at',
+    issuer: 'Codecademy',
+    imgUrl: '/assets/certifications/sass-cert.avif',
+    certUrl:
+      'https://www.codecademy.com/profiles/pjnalls/certificates/eb1ffda40f347629dcef6de33d3f9741',
+  },
+  {
+    alt: 'Completed scripting language certification',
+    name: 'Certified JavaScript Developer',
+    description: 'Verify completion at',
+    issuer: 'W3Schools',
+    imgUrl: '/assets/certifications/javascript-cert.avif',
+    certUrl: 'https://verify.w3schools.com/1ODZHSV1OV',
+  },
+  {
+    alt: 'Completed Japanese certification',
+    name: 'N2 Certificate Japanese-Language Proficiency',
+    description: 'Verify completion with ID "N2A156406A"',
+    issuer: 'The Japan Foundation / Japan Educational Exchanges and Services',
+    imgUrl: '/assets/certifications/japanese-cert.avif',
+  },
+  {
+    alt: 'Completed Chinese certification',
+    name: 'Certificate of Achievement for Chinese',
+    description:
+      'for successfully completing 40 hours of Chinese lessons with certified Preply tutors.',
+    issuer: 'Preply',
+    imgUrl: '/assets/certifications/chinese-cert.avif',
+  },
+  {
+    alt: '數位轉型趨勢-跨平台 APP DIY 實作課程',
+    name: 'Certificate of Completion',
+    description: '我重新學習了如何使用 Facebook 官方開發框架 React Native 。',
+    issuer: 'Udemy',
+    imgUrl: '/assets/certifications/app-diy.avif',
+  },
 ];
