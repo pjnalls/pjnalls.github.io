d977287e0d0806c918f1f0e5d99ccd6eb401734d
pjnalls <42269808+pjnalls@users.noreply.github.com>
2024-11-16 22:52:58 -0600

    🚀 deploy(rewrite): ship first version of new website ✨

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://pjnalls.github.io',
     integrations: [mdx(), sitemap(), tailwind(), react()],
 });
