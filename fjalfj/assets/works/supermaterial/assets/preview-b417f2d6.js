9c7d3a2eae48647b652131e23c8ea32096272a29
pjnalls <42269808+pjnalls@users.noreply.github.com>
2024-11-16 22:09:10 -0600

    🆕 feat(rewrite): setup light and dark themes; finish building homepage ✨

--- fjalfj/assets/works/supermaterial/assets/preview-b417f2d6.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-0f894a73.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-65dd9e9d.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-65dd9e9d.js","./iframe-0f894a73.js","./index-c6dae603.js","./_getPrototype-c43a1d6a.js","./chunk-4NMOSTKD-938b085e.js","./index-d475d2ea.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-b417f2d6.js.map
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { FaMoon } from 'react-icons/fa';
+import { IoIosSunny } from 'react-icons/io';
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState('light');
+
+  const handleClick = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  useEffect(() => {
+    if (window) {
+      setTheme(window.localStorage.getItem('theme') ?? 'light');
+    } else {
+      setTheme('light');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+    window.localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <button onClick={handleClick} className='inline-block align-middle'>
+      <span className="sr-only">Light and Dark Theme Switch</span>
+      {theme === 'light' ? <FaMoon size={24}/> : <IoIosSunny size={24}/>}
+    </button>
+  );
+}
