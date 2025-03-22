6f85b33966dda2db30a466d50b322819a0f781fa
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-03-22 05:27:13 -0500

    🆕 content(blog) add new post about a paradox ✨; misc

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Preston's Portfolio";
-export const SITE_HOME_TITLE = "Preston's Portfolio | Home";
-export const SITE_PROJECTS_TITLE = "Preston's Portfolio | Projects";
-export const SITE_BLOG_TITLE = "Preston's Portfolio | Blog";
-export const SITE_ABOUT_TITLE = "Preston's Portfolio | About";
+export const SITE_TITLE = "Preston's Portfolio and Blog";
+export const SITE_HOME_TITLE = "Preston's Portfolio and Blog | Home";
+export const SITE_PROJECTS_TITLE = "Preston's Portfolio and Blog | Projects";
+export const SITE_BLOG_TITLE = "Preston's Portfolio and Blog | Blog";
+export const SITE_ABOUT_TITLE = "Preston's Portfolio and Blog | About";
 export const SITE_DESCRIPTION =
-  'This is the portfolio website of Preston Nalls a React Native frontend web and mobile application engineer.';
+  'This is the portfolio and blog website of Preston Nalls a Senior Software Engineer and Tutor.';
---
+++ src/content/blog/paradox-of-progress.md
@@ -0,0 +1,12 @@
+---
+title: Paradox of Progress
+description: Here's a short post I wrote expressing a paradox I noticed within people and society.
+pubDate: 'Mar 22 2025'
+heroImage: '/posts/paradox-of-progress.jpg'
+---
+
+Scientific and technological discoveries in ancient times were often veiled in mystery, before the advent of widespread communication and technological advancements.
+<br/>However, was the human psyche from our past integrated within society?
+
+Conversely, scientific and technological discoveries in today's times are often integrated within society.
+<br/>However, is the human psyche still veiled in mystery, after the advent of widespread communication and technological advancements?
