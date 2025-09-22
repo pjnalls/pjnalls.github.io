2d323f2c9fb14966e2ef86c8900e2eca46cbf095
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-09-22 06:29:30 -0500

    chore(bio): update info to sync up with profiles on different mediums

--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,7 @@ export const SITE_PROJECTS_TITLE = "Preston's Portfolio and Blog | Projects";
 export const SITE_BLOG_TITLE = "Preston's Portfolio and Blog | Blog";
 export const SITE_ABOUT_TITLE = "Preston's Portfolio and Blog | About";
 export const SITE_DESCRIPTION =
-  'This is the portfolio and blog website of Preston Nalls, a full-stack, cross-platform, and AI/ML software engineer and Master of Science in Information Technology postgraduate at the University of the People.';
+  'This is the portfolio and blog website of Preston Nalls, a software engineer and polyglot proficient in a variety of programming and natural languages.';
 
 export const LANGUAGES = [
   {
