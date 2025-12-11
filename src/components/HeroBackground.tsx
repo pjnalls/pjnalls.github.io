4ec8553b0d540fd81fe3f84abd68c00a87a2e0b8
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-12-10 22:39:04 -0600

    fix(hero): resolve video hero width & height

--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -3,16 +3,15 @@ import ReactPlayer from 'react-player';
 export const HeroBackground = () => {
   return (
     <>
-      <div className='relative bg-transparent'>
+      <div className="relative bg-transparent">
         <ReactPlayer
           src="/hero-movie.mp4"
           playing={true}
           loop={true}
           muted={true}
           controls={false}
-          width="100%"
-          height="100%"
-          className="z-0 fixed mx-auto top-[-31.5vh] xs:[-30vh] sm:top-[-26vh] md:top-[-21.5vh] lg:top-[-12.5vh] bg-transparent"
+          style={{ width: '100%', height: 'auto', aspectRatio: '16/9' }}
+          className="z-0 fixed mx-auto top-[0vh] xs:[0vh] sm:top-[0vh] md:top-[0vh] lg:top-[0vh] bg-transparent"
         />
       </div>
       <div className="z-10 fixed mx-auto w-full h-full bg-[#cfb7ffb0] dark:bg-[#1f074fb0] top-0"></div>
